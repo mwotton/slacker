e7645545c587b0e4eddbb689ecc8acc4fbee35c1
Mark Wotton <mwotton@gmail.com>
2016-06-13 13:05:19 -0400

    switch to other slack lib

--- slacker/Main.hs
+++ slacker/Main.hs
@@ -1,11 +1,37 @@
-import           Control.Applicative ((<$>))
-import           Network.Slack
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+import           Control.Applicative       ((<$>))
+import           Control.Monad.IO.Class    (liftIO)
+import           Control.Monad.State.Class
+
+import qualified Data.Text                 as T
 import           System.Directory
-import           System.Environment  (getArgs)
+import           System.Environment        (getArgs)
+import           System.Exit
+import qualified Web.Slack                 as S
+import qualified Web.Slack.Message         as S
+import qualified Web.Slack.Types.Id        as SI
+
+echoBot user slack channel stuff S.Hello = do
+  liftIO $ print "hello"
+  r <- map S._channelId . filter ((=="automated_alerts") . S._channelName ) . S._slackGroups . S._session <$> get
+  case r of
+    [] -> liftIO $ print ("no such channel", channel) >> exitFailure
+    [r] -> do
+      S.sendMessage r (T.pack $ concat stuff)
+      liftIO exitSuccess
+
+echoBot a b c d e = liftIO $ print "somethingelse" >> print (a,b,c,d,e)
 
 main :: IO ()
 main = do
   (user:slack:channel:stuff) <- getArgs
+  print "doin stuff"
   token <- (head . lines) <$> (readFile . (++("/.slack/tokens/" ++ slack)) =<< getHomeDirectory)
-  print =<< (runSlack token $
-    postMessage user (unwords stuff) =<<     channelFromName channel)
+ --  Right x <- runSlack token $ channels
+--  mapM_ print x
+
+  S.runBot (S.SlackConfig token) (echoBot user slack channel stuff) ()
+
+--   print =<< (runSlack token $ (postMessage user (unwords stuff) =<<     channelFromName channel))
